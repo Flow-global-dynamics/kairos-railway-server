f12c43b9ba43c2915eff6148daeb0f4028823579
Flow-global-dynamics <flowglobal.admin@proton.me>
2025-04-29 17:28:02 +0200

    Delete utils_directory.py

--- utils_directory.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-"""
-Package d'utilitaires pour l'application FlowGlobalApp
-"""
-
-# Ce fichier est nécessaire pour que Python traite le répertoire utils comme un package
