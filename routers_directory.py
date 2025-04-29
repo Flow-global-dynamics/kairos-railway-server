e8f3a8013df4bdcdb3de624c5c220432801242c1
Flow-global-dynamics <flowglobal.admin@proton.me>
2025-04-29 17:28:44 +0200

    Delete routers_directory.py

--- routers_directory.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-"""
-Package de routers pour l'application FlowGlobalApp
-"""
-
-# Ce fichier est nécessaire pour que Python traite le répertoire routers comme un package
